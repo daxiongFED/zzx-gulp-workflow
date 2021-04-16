4258a10b49fa7ee3782900a259ebfcfde25b501b
大雄FED <8859186+daxiong-fed@user.noreply.gitee.com>
2021-04-16 21:16:41 +0800

    feat: update package

---
+++ bin/zzx-gulp-workflow.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log('zzx-gulp-workflow')
+
+
+process.argv.push('--cwd')
+process.argv.push(process.cwd())
+process.argv.push('--gulpfile')
+process.argv.push(require.resolve('../lib/index.js')) // 也可以写成 require.resolve('..') ，因为package.json 的 main 指向了 lib/index.js
+
+// console.log(process.argv);
+
+require('gulp/bin/gulp')
